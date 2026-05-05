b0b8dde9aa68b4eb262915e2a1f1965036076bcc
Yael Ram <Drivooshtech@gmail.com>
2026-05-05 18:39:22 +0300

    design updates and logic

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+dotenv.config();
+
 import sequelize from "./src/config/db.js";
 import "./src/models/index.js";
 import userRouter from "./src/routes/authRoutes.js"
@@ -9,7 +11,6 @@ import studentRouter from "./src/routes/studentRoutes.js";
 import bookingRouter from "./src/routes/bookingRoutes.js";
 import reviewRouter from "./src/routes/reviewRoutes.js";
 
-dotenv.config();
 
 const app = express();
 
--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,25 +1,49 @@
+// import { Sequelize } from 'sequelize';
+// import dotenv from 'dotenv';
+
+
+// dotenv.config();
+
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     dialect: 'postgres',
+//     logging: false,
+//     pool: {
+//       max: 5,
+//       min: 0,
+//       acquire: 30000,
+//       idle: 10000
+//     }
+//   }
+// );
+
+// export default sequelize;
+
 import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
 
-
 dotenv.config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: 'postgres',
-    logging: false,
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
     }
+  },
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
   }
-);
+});
 
 export default sequelize;
