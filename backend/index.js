8b72374670dadba2d1fb0462d472cc1fb7d62946
yael3259 <157998716+yael3259@users.noreply.github.com>
2026-07-27 13:09:05 +0300

    fix server port login bugs

--- backend/index.js
+++ backend/index.js
@@ -62,9 +62,3 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`server is running on port ${PORT}`);
 });
-
-// const PORT = Number(process.env.PORT) || 5000;
-
-// app.listen(PORT, "0.0.0.0", () => {
-//     console.log(`Server running on port ${PORT}`);
-// });
